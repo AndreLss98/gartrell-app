af2cf93df91d21a0d93e475096a68c95f2789a65
João Victor da Silva <joaovictor00788123@gmail.com>
2021-03-20 12:24:13 -0300

    Criando modulo servicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { ServicosComponent } from './modules/servicos/servicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ServicosComponent
   ],
   imports: [
     BrowserModule,
