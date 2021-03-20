d90f655501fa3d42e870c6b21bc3f9c9350ae9ef
Victor Torres <victoorres@icloud.com>
2021-03-20 15:27:53 -0300

    Adicionando o component EVENTO dentro de ACONTECE

--- src/app/modules/acontece/acontece.module.ts
+++ src/app/modules/acontece/acontece.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { AconteceComponent } from './acontece.component';
 import { AconteceRoutingModule } from './acontece-routing.module';
+import { EventoComponent } from 'src/app/shared/components/evento/evento.component';
 
 @NgModule({
   declarations: [ AconteceComponent ],
