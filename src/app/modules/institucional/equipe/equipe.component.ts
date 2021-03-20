878f18db59b45191e5bf95648accfaa196de8cca
Victor Torres <victoorres@icloud.com>
2021-03-20 18:44:30 -0300

    Componente EQUIPE atualizado.

--- src/app/modules/institucional/equipe/equipe.component.ts
+++ src/app/modules/institucional/equipe/equipe.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipeComponent implements OnInit {
 
-  constructor() { }
+  private _equipe: any[] = [];
+  private _colaboradores: any[] = [];
+
+  constructor() {}
 
   ngOnInit(): void {
+
+    this.equipe = [
+      {
+        name: "Elóide A. A. Botelho",
+        cargo: "Psicóloga; CRP - 09/3124",
+        bio: `Psicóloga - Área Cognitivo Comportamental e Processual;
+        Esp, em Neuropsicologia e Avaliação Psicológica; Atende;
+        Adolescentes, jovens e adultos.`,
+        img: 'Elóide A. A. Botelho.jpg'
+      }
+    ],
+
+    this.colaboradores = [
+      {
+        name: "Eleny Vassão",
+        cargo: "Capelã Evangélica/Consultora em Capelania Cristã",
+        bio: `Formada em Artes Plásticas e Teologia
+        Mestre em Aconselhamento Bíblico
+        Escritora
+        Exerceu capelania na USP/SP por 22 anos
+        Consultora em Capelania
+        Capelã Evangélica TÍt. Do I. I. Emílio Ribas-SP e HSPE
+        Diretora geral da ACS
+        Atualmente, Diretora Geral da ACS e Capelã - Missionária pelo Supremo Concilio da I. P. B`,
+        img: 'eleny_vassao.jpg'
+      }
+    ]
   }
 
+  public get equipe(): any[] {
+    return this._equipe;
+  }
+
+  public get colaboradores(): any[]{
+    return this._colaboradores;
+  }
+
+  public set equipe(value: any[]) {
+    this._equipe = value;
+  }
+
+  public set colaboradores(value: any[]) {
+    this._colaboradores = value;
+  }  
 }
