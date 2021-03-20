b85de72bc34a5a41a10c3528cf201d52b461b0ea
João Victor da Silva <joaovictor00788123@gmail.com>
2021-03-20 14:14:23 -0300

    Criando component contato-form

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelfImageComponent } from './self-image/self-image.component';
 import { ServicoCircleComponent } from './servico-circle/servico-circle.component';
+import { ContatoFormComponent } from './contato-form/contato-form.component';
 
 @NgModule({
-  declarations: [ SelfImageComponent, ServicoCircleComponent ],
+  declarations: [ SelfImageComponent, ServicoCircleComponent, ContatoFormComponent ],
   imports: [
     CommonModule
   ],
-  exports: [ SelfImageComponent, ServicoCircleComponent ]
+  exports: [ SelfImageComponent, ServicoCircleComponent, ContatoFormComponent ]
 })
 export class ComponentsModule { }
