411cf4844a88b23b925ba24c8265f2fa5361fb8e
João Victor da Silva <joaovictor00788123@gmail.com>
2021-03-20 14:11:34 -0300

    Adicionando component servico-circle no modulo servico

--- src/app/modules/servicos/servicos.component.ts
+++ src/app/modules/servicos/servicos.component.ts
@@ -8,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class ServicosComponent implements OnInit {
 
   constructor() { }
-
+  private _servicesCategory: any[] = [];
+  
   ngOnInit(): void {
+    this.servicesCategory = [
+      { nome: 'Psiquiatria' },
+      { nome: 'Psicologia' },
+      { nome: 'Neuropsicologia' },
+      { nome: 'Grupos de Suporte e Crescimento Pessoal' },
+      { nome: 'Capacitações' },
+      { nome: 'Consultoria' },
+      // { nome: 'Parcerias ou Acordo de Cooperação em Serviços, Atividades e Projetos' }
+    ]
+  }
+
+  public get servicesCategory(): any[] {
+    return this._servicesCategory;
+  }
+
+  public set servicesCategory(value: any[]) {
+    this._servicesCategory = value;
   }
 
 }
