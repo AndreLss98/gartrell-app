268ca1bb741822c13a3018b95d1e59e77322f02f
João Victor da Silva <joaovictor00788123@gmail.com>
2021-03-20 15:41:28 -0300

    Criando component PostComponent

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SelfImageComponent } from './self-image/self-image.component';
 import { ServicoCircleComponent } from './servico-circle/servico-circle.component';
 import { ContatoFormComponent } from './contato-form/contato-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [ SelfImageComponent, ServicoCircleComponent, ContatoFormComponent ],
+  declarations: [ SelfImageComponent, ServicoCircleComponent, ContatoFormComponent, PostComponent ],
   imports: [
     CommonModule
   ],
-  exports: [ SelfImageComponent, ServicoCircleComponent, ContatoFormComponent ]
+  exports: [ SelfImageComponent, ServicoCircleComponent, ContatoFormComponent, PostComponent ]
 })
 export class ComponentsModule { }
