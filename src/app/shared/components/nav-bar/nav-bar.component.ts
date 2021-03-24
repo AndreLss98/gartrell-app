e8e4eb1a510191ec1014c1a73e9d0e1af3e4b9fc
ANDRÉ <andreluiss@ufg.br>
2021-03-24 17:58:03 -0300

    Altera rotas dos menus de servicos

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -16,20 +16,20 @@ export class NavBarComponent implements OnInit {
         { name: "Identidade Nominal", link: "", subMenus: [] },
         { name: "Quem somos", link: "institucional/quem-somos", subMenus: []},
         { name: "Equipe", link: "", subMenus: [
-          { name: "Instituto Gartrell", link: "institucional/equipe", subMenus: []},
-          { name: "Parceiros e Colaboradores", link: "institucional/equipe", subMenus: []}
+          { name: "Instituto Gartrell", link: "institucional/equipe#equipe", subMenus: []},
+          { name: "Parceiros e Colaboradores", link: "institucional/equipe#parceiros", subMenus: []}
         ] },
         { name: "Galeria de Fotos", link: "", subMenus: [] },
         { name: "Links úteis", link: "", subMenus: [] }
       ]},
       { name: "Serviços", link: "", subMenus: [
-        { name: "Psiquiatria", link: "servico/psiquiatria", subMenus: [] },
-        { name: "Psicologia", link: "servico/psicologia", subMenus: [] },
-        { name: "Neuropsicologia", link: "servico/neuropsicologia", subMenus: [] },
-        { name: "Grupos de suporte e crescimento pessoal", link: "servico/grupo-suporte-crescimento-pessoal", subMenus: [] },
-        { name: "Capacitações", link: "servico/capacitacoes", subMenus: [] },
-        { name: "Consultoria", link: "servico/consultoria", subMenus: [] },
-        { name: "Parcerias ou Acordos de Cooperação em Serviços, Atividades e Projetos", link: "servico/parceria-acordos", subMenus: [] }
+        { name: "Psiquiatria", link: "servicos/psiquiatria", subMenus: [] },
+        { name: "Psicologia", link: "servicos/psicologia", subMenus: [] },
+        { name: "Neuropsicologia", link: "servicos/neuropsicologia", subMenus: [] },
+        { name: "Grupos de suporte e crescimento pessoal", link: "servicos/grupo-suporte-crescimento-pessoal", subMenus: [] },
+        { name: "Capacitações", link: "servicos/capacitacoes", subMenus: [] },
+        { name: "Consultoria", link: "servicos/consultoria", subMenus: [] },
+        { name: "Parcerias ou Acordos de Cooperação em Serviços, Atividades e Projetos", link: "servicos/parceria-acordos", subMenus: [] }
       ]},
       { name: "Acontece", link: "acontece", subMenus: [] },
       { name: "Notícias", link: "noticias", subMenus: [] },
