e99c7734c9a55bddda9d6949a211bb9382cb64cb
ANDRÉ <andreluiss@ufg.br>
2021-03-25 18:47:12 -0300

    Estrutura e estiliza pagina de reflexoes

--- src/app/modules/reflexoes/reflexoes.component.ts
+++ src/app/modules/reflexoes/reflexoes.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReflexoesComponent implements OnInit {
 
+  private _reflexoes: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.reflexoes = [
+      { 
+        titulo: 'Lorem ipsumn dolor sit amet, consecutur adipiscing elit.',
+        resumo: 'Lorem ipsumn dolor sit amet, consecutur adipiscing elit. Lorem ipsumn dolor sit amet, consecutur adipiscing elit.',
+        autor: 'Karina Queiroz',
+        dataPost: new Date(2020, 1, 22),
+        banner: 'assets/home/noticias/image_placeholder.jpg'
+      },
+      { 
+        titulo: 'Lorem ipsumn dolor sit amet, consecutur adipiscing elit.',
+        resumo: 'Lorem ipsumn dolor sit amet, consecutur adipiscing elit. Lorem ipsumn dolor sit amet, consecutur adipiscing elit.',
+        autor: 'Karina Queiroz',
+        dataPost: new Date(2020, 3, 9),
+        banner: 'assets/home/noticias/image_placeholder.jpg'
+      },
+      { 
+        titulo: 'Lorem ipsumn dolor sit amet, consecutur adipiscing elit.',
+        resumo: 'Lorem ipsumn dolor sit amet, consecutur adipiscing elit. Lorem ipsumn dolor sit amet, consecutur adipiscing elit.',
+        autor: 'Karina Queiroz',
+        dataPost: new Date(2020, 11, 25),
+        banner: 'assets/home/noticias/image_placeholder.jpg'
+      }
+    ]
+  }
+
+  public get reflexoes(): any[] {
+    return this._reflexoes;
+  }
+
+  public set reflexoes(value: any[]) {
+    this._reflexoes = value;
   }
 
 }
