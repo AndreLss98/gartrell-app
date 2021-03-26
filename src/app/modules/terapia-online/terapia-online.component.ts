5f5739a2c2bff9248718a5d81cef0d89ffbcf9eb
ANDRÉ <andreluiss@ufg.br>
2021-03-25 22:07:42 -0300

    Estiliza pagina de terapia online

--- src/app/modules/terapia-online/terapia-online.component.ts
+++ src/app/modules/terapia-online/terapia-online.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerapiaOnlineComponent implements OnInit {
 
+  private _profissionais: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.profissionais = [
+      {
+        img: 'assets/home/equipe/eleny_vassao.jpg',
+        nome: 'Nome Sobrenome Aqui',
+        profissao: 'Psicólogia',
+        bio: 'Ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.',
+        horarioDisponivel: '08:00h às 18:00h'
+      },
+      {
+        img: 'assets/home/equipe/eleny_vassao.jpg',
+        nome: 'Nome Sobrenome Aqui',
+        profissao: 'Psicólogia',
+        bio: 'Ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.',
+        horarioDisponivel: '08:00h às 18:00h'
+      },
+      {
+        img: 'assets/home/equipe/eleny_vassao.jpg',
+        nome: 'Nome Sobrenome Aqui',
+        profissao: 'Psicólogia',
+        bio: 'Ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.',
+        horarioDisponivel: '08:00h às 18:00h'
+      }
+    ]
+  }
+
+  public get profissionais(): any[] {
+    return this._profissionais;
+  }
+
+  public set profissionais(value: any[]) {
+    this._profissionais = value;
   }
 
 }
