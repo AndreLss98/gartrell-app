2b8d5a31075b10f01b6feca3367476f0471bdd9f
Victor Torres <victoorres@icloud.com>
2021-03-26 19:04:05 -0300

    Alterações no CSS

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-nav-bar',
@@ -13,47 +13,55 @@ export class NavBarComponent implements OnInit {
   constructor(private router: Router) {
 
     this.menu = [
-      { name: "Institucional", link: "", subMenus: [
-        { name: "Identidade Nominal", link: "institucional/identidade-nominal", subMenus: [] },
-        { name: "Quem somos", link: "institucional/quem-somos", subMenus: []},
-        { name: "Equipe", link: "", subMenus: [
-          { name: "Instituto Gartrell", link: "institucional/equipe#equipe", subMenus: []},
-          { name: "Parceiros e Colaboradores", link: "institucional/equipe#parceiros", subMenus: []}
-        ] },
-        { name: "Galeria de Fotos", link: "institucional/galeria-fotos", subMenus: [] },
-        { name: "Links úteis", link: "", subMenus: [] }
-      ]},
-      { name: "Serviços", link: "", subMenus: [
-        { name: "Psiquiatria", link: "servicos/1", subMenus: [] },
-        { name: "Psicologia", link: "servicos/2", subMenus: [] },
-        { name: "Neuropsicologia", link: "servicos/3", subMenus: [] },
-        { name: "Grupos de suporte e crescimento pessoal", link: "servicos/4", subMenus: [] },
-        { name: "Capacitações", link: "servicos/5", subMenus: [] },
-        { name: "Consultoria", link: "servicos/6", subMenus: [] },
-        { name: "Parcerias ou Acordos de Cooperação em Serviços, Atividades e Projetos", link: "servicos/7", subMenus: [] }
-      ]},
-      { name: "Acontece", link: "acontece", subMenus: [] },
-      { name: "Notícias", link: "noticias", subMenus: [] },
-      { name: "Reflexões", link: "reflexoes", subMenus: [] },
-      { name: "Fique por Dentro", link: "", subMenus: [
-        { name: "Saúde Integral", link: "fique-por-dentro/saude-integral", subMenus: [] },
-        { name: "Desenvolvimento humano", link: "fique-por-dentro/desenvolvimento-humano", subMenus: [] },
-        { name: "Sociedade", link: "fique-por-dentro/sociedade", subMenus: [] }
-      ]},
-      { name: "Contato", link: this.goToContactForm(), subMenus: [] }
+      {
+        name: "INSTITUCIONAL", link: "", subMenus: [
+          { name: "Identidade Nominal", link: "institucional/identidade-nominal", subMenus: [] },
+          { name: "Quem somos", link: "institucional/quem-somos", subMenus: [] },
+          {
+            name: "Equipe", link: "", subMenus: [
+              { name: "Instituto Gartrell", link: "institucional/equipe#equipe", subMenus: [] },
+              { name: "Parceiros e Colaboradores", link: "institucional/equipe#parceiros", subMenus: [] }
+            ]
+          },
+          { name: "Galeria de Fotos", link: "institucional/galeria-fotos", subMenus: [] },
+          { name: "Links úteis", link: "", subMenus: [] }
+        ]
+      },
+      {
+        name: "SERVIÇOS", link: "", subMenus: [
+          { name: "Psiquiatria", link: "servicos/1", subMenus: [] },
+          { name: "Psicologia", link: "servicos/2", subMenus: [] },
+          { name: "Neuropsicologia", link: "servicos/3", subMenus: [] },
+          { name: "Grupos de suporte e crescimento pessoal", link: "servicos/4", subMenus: [] },
+          { name: "Capacitações", link: "servicos/5", subMenus: [] },
+          { name: "Consultoria", link: "servicos/6", subMenus: [] },
+          { name: "Parcerias ou Acordos de Cooperação em Serviços, Atividades e Projetos", link: "servicos/7", subMenus: [] }
+        ]
+      },
+      { name: "ACONTECE", link: "acontece", subMenus: [] },
+      { name: "NOTÍCIAS", link: "noticias", subMenus: [] },
+      { name: "REFLEXÕES", link: "reflexoes", subMenus: [] },
+      {
+        name: "FIQUE POR DENTRO", link: "", subMenus: [
+          { name: "Saúde Integral", link: "fique-por-dentro/saude-integral", subMenus: [] },
+          { name: "Desenvolvimento humano", link: "fique-por-dentro/desenvolvimento-humano", subMenus: [] },
+          { name: "Sociedade", link: "fique-por-dentro/sociedade", subMenus: [] }
+        ]
+      },
+      { name: "CONTATO", link: this.goToContactForm(), subMenus: [] }
     ]
   }
 
   ngOnInit(): void {
     const temp = document.getElementsByClassName('dropdown-menu');
-    for (let i = 0; i < temp.length; ++i ) {
+    for (let i = 0; i < temp.length; ++i) {
       temp.item(i)?.addEventListener('click', (event) => {
         event.stopPropagation();
       })
     }
   }
 
-  goToTerapiaOnline(){
+  goToTerapiaOnline() {
     this.router.navigateByUrl("/terapia-online")
   }
 
