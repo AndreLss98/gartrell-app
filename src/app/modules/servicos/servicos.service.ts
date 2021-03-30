5dd10e4f1dbbe6da7a9602883947977317bc6bbb
ANDRÉ <andreluiss@ufg.br>
2021-03-30 15:30:38 -0300

    Ajusta textos de descricao dos servicos

--- src/app/modules/servicos/servicos.service.ts
+++ src/app/modules/servicos/servicos.service.ts
@@ -11,43 +11,43 @@ export class ServicosService {
       id: 1,
       nome: 'Psiquiatria',
       icone: 'assets/svg/psiquiatria.svg',
-      descricao: 'Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+      descricao: 'Indicada para qualquer pessoa (crianças, adolescentes, jovens e adultos) que lida com diferentes formas de sofrimento psíquico ou mental, sejam elas de base orgânica ou funcional, com manifestações leves, moderadas ou graves, em situações de crises agudas ou crônicas.'
     },
     {
       id: 2,
       nome: 'Psicologia',
       icone: 'assets/svg/psicologia.svg',
-      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+      descricao: 'Acompanhamento Psicoterápico (Psicoterapia, para todas as idades a partir de 7 anos); Orientação Vocacional/Profissional; Apoio ao Vestibulando, Concurseiro; Colocação/Recolocação Profissional, Readequação/Reorientação de Carreira.'
     },
     {
       id: 3,
       nome: 'Neuropsicologia',
       icone: 'assets/svg/neuropsicologia.svg',
-      descricao: 'Etiam rutrum erat nec quam tincidunt, sed commodo lorem consequat. Cras et vulputate lacus. Nullam lacinia vitae nisl a tempor. In hac habitasse platea dictumst. Donec nulla ligula, molestie aliquet lorem vel, consectetur porttitor nisi. Nulla mattis, sem eget dignissim efficitur, magna lorem fermentum nibh, eget tincidunt purus libero quis lorem.'
+      descricao: 'Identificar a presença ou não de disfunção cognitiva; auxiliar no diagnóstico diferencial; identificar e compreender quais habilidades estão preservadas, após um dado quadro neurológico, psiquiátrico, neuropsiquiátrico ou de desenvolvimento; acompanhar a evolução do quadro em relação ao tratamento medicamentoso e cirúrgico; acompanhar a evolução da reabilitação; documentar o déficit cognitivo'
     },
     {
       id: 4,
       nome: 'Grupos de Suporte e Crescimento Pessoal',
       icone: 'assets/svg/grupos_de_suporte_e_crescimento_pessoal.svg',
-      descricao: 'Aenean iaculis libero odio, quis euismod sem elementum ac. Nam a aliquam lorem. Morbi ut erat libero. Aenean imperdiet diam ut laoreet egestas. Nunc laoreet quis mi et pharetra. Nulla feugiat nec nulla finibus tincidunt. Sed at finibus ante. Quisque ut odio felis.'
+      descricao: 'Destinados às pessoas que passam por dificuldades semelhantes ou partilham de momentos ou interesses comuns. Estes grupos permitirão aos participantes compartilharem suas experiências, desejos, aspirações e metas, bem como suas necessidades, inseguranças, aflições, medos, dúvidas, problemas, etc. Desenvolvidos em 10 a 15 encontros semanais com duração de 1h30 minutos.'
     },
     {
       id: 5,
       nome: 'Capacitações',
       icone: 'assets/svg/capacitacoes_icon.svg',
-      descricao: 'Etiam tincidunt tincidunt ante eu pellentesque. Nulla id nisl interdum, fermentum ante ut, scelerisque quam. Sed at mattis nisl, nec scelerisque mauris. Suspendisse pretium dolor nec diam interdum ultrices. Aliquam porttitor mauris tincidunt lacus lacinia, non fermentum est vulputate. Suspendisse tincidunt luctus nisl, id feugiat ligula mollis a.'
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
     },
     {
       id: 6,
       nome: 'Consultoria',
       icone: 'assets/svg/consultoria_icon.svg',
-      descricao: 'Curabitur tristique diam at turpis congue bibendum. Quisque libero erat, venenatis non ante ut, congue viverra nunc. Sed imperdiet augue non fringilla ornare. Aliquam tempus auctor facilisis. Nullam fringilla euismod urna, vitae pulvinar nulla rutrum eget. Suspendisse potenti. Phasellus in nibh gravida, pulvinar ipsum a, dictum erat.'
+      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
     },
     {
       id: 7,
       nome: 'Parcerias ou Acordos de Cooperação em Serviços, Atividades e Projetos',
       icone: 'assets/svg/parcerias_ou_acordos_de_cooperacao.svg',
-      descricao: 'Curabitur tristique diam at turpis congue bibendum. Quisque libero erat, venenatis non ante ut, congue viverra nunc. Sed imperdiet augue non fringilla ornare. Aliquam tempus auctor facilisis. Nullam fringilla euismod urna, vitae pulvinar nulla rutrum eget. Suspendisse potenti. Phasellus in nibh gravida, pulvinar ipsum a, dictum erat.'
+      descricao: 'O Instituto Gartrell tem buscado estabelecer PARCERIAS OU ACORDOS DE COOPERAÇÃO com profissionais e instituições de saúde, de educação e com entidades sociais e religiosas verdadeiramente comprometidas com a ética (valores nobres e elevados) e a cidadania, visando à troca de saberes, experiências e a realização conjunta de ações psicoeducativas (ministração de cursos/capacitações, treinamentos, palestras, consultorias, eventos, etc.).'
     }
   ];
 
