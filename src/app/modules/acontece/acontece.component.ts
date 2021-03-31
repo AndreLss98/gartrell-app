38969d4f718d3b6f6ee17b90195792714488fb21
ANDRÉ <andreluiss@ufg.br>
2021-03-31 15:20:47 -0300

    Busca acontecimentos apartir da camada de service

--- src/app/modules/acontece/acontece.component.ts
+++ src/app/modules/acontece/acontece.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AconteceService } from './acontece.service';
 
 @Component({
   selector: 'app-acontece',
@@ -7,24 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AconteceComponent implements OnInit {
 
-  private _acontecimentos: any[] = [];
-
-  constructor() { }
+  constructor(
+    public aconteceService: AconteceService
+  ) { }
 
   ngOnInit(): void {
-    this.acontecimentos = [
-      { dia: 18, mes: 'Novemnbro', titulo: 'IV Jornada de Saúde Mental', descricao: 'Pandemia Global e Fé Crisã', banner: 'assets/home/evento/evento_banner.jpg' },
-      { dia: 25, mes: 'Dezembro', titulo: 'Nome do Evento aqui!', descricao: 'Descrição do evento aqui.', banner: 'assets/home/evento/evento_banner.jpg' },
-      { dia: 27, mes: 'Dezembro', titulo: 'Nome do Evento aqui!', descricao: 'Descrição do evento aqui.', banner: 'assets/home/evento/evento_banner.jpg' },
-      { dia: 31, mes: 'Dezembro', titulo: 'Nome do Evento aqui!', descricao: 'Descrição do evento aqui.', banner: 'assets/home/evento/evento_banner.jpg' }
-    ]
-  }
-
-  public get acontecimentos(): any[] {
-    return this._acontecimentos;
-  }
-
-  public set acontecimentos(value: any[]) {
-    this._acontecimentos = value;
+    
   }
 }
