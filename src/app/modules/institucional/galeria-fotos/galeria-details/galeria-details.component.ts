296e993340b33fced53000961b48d514ae4bfa1d
ANDRÉ <andreluiss@ufg.br>
2021-04-12 19:39:11 -0300

    cria componete para view detalhada da galeria

--- src/app/modules/institucional/galeria-fotos/galeria-details/galeria-details.component.ts
+++ src/app/modules/institucional/galeria-fotos/galeria-details/galeria-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Galeria } from 'src/app/models/galeria-foto.model';
 
 @Component({
   selector: 'app-galeria-details',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaDetailsComponent implements OnInit {
 
-  constructor() { }
+  private _galeria: Galeria;
+  private _selectedImg: number = 0;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.galeria = this.route.snapshot.data.galeria;
+  }
+
+  public get galeria(): Galeria {
+    return this._galeria;
+  }
+  public set galeria(value: Galeria) {
+    this._galeria = value;
+  }
+
+  public get selectedImg(): number {
+    return this._selectedImg;
+  }
+  public set selectedImg(value: number) {
+    this._selectedImg = value;
+  }
+
+  selectImg(data: any) {
+    this.selectedImg = data.currentPage - 1;
   }
 
 }
