6d6069f6065a040fe158a82853f353a975d3d351
ANDRÉ <andreluiss@ufg.br>
2021-04-13 18:31:41 -0300

    cria pagina de links uteis e ajusta links do roda pe do site

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LinksUteisService } from 'src/app/modules/institucional/links-uteis/links-uteis.service';
+import { LinkUtil } from '../../models/links-uteis.model';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,12 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private _firstsLinks: LinkUtil[] = [];
+
   constructor(
-    
+    public linksUteisService: LinksUteisService
   ) { }
 
   ngOnInit(): void {
-    
+    this.firstsLinks = this.linksUteisService.linksUteis.slice(0, 4);
+  }
+
+  public get firstsLinks(): LinkUtil[] {
+    return this._firstsLinks;
+  }
+  public set firstsLinks(value: LinkUtil[]) {
+    this._firstsLinks = value;
   }
 
   backToTop(): string {
