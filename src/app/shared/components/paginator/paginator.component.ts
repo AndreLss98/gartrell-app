3631b49be88a755a38d0f476ec802a75b07a7984
ANDRÉ <andreluiss@ufg.br>
2021-04-13 19:25:13 -0300

    Melhora usabilidade do componente de paginacao

--- src/app/shared/components/paginator/paginator.component.ts
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -15,11 +15,13 @@ export class PaginatorComponent implements OnInit {
 
   private _selected: number = 1;
   private _pages: number[] = [];
+  private _viewPages: number[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.pages = Array.from({ length: this.length }, (_, i) => i+1);
+    this.pages = Array.from({ length: this.length }, (_, i) => i + 1);
+    this.viewPages = this.pages.slice(0, 4);
   }
 
   public get selected(): number {
@@ -36,18 +38,34 @@ export class PaginatorComponent implements OnInit {
     this._pages = value;
   }
 
+  public get viewPages(): number[] {
+    return this._viewPages;
+  }
+  public set viewPages(value: number[]) {
+    this._viewPages = value;
+  }
+
   public selectPage(currentPage: number) {
     this.selected = currentPage;
+    
+    this.viewPages = this.pages.slice(currentPage - 1, currentPage + 3);
+    
     this.changePage.emit({ currentPage });
   }
 
   public goNext() {
     if(this.selected < this.length) this.selected++;
+
+    this.viewPages = this.pages.slice(this.selected - 1, this.selected + 3);
+
     this.changePage.emit({ currentPage: this.selected });
   }
 
   public goPrevious() {
     if (this.selected > 1) this.selected--;
+
+    this.viewPages = this.pages.slice(this.selected - 1, this.selected + 3);
+
     this.changePage.emit({ currentPage: this.selected });
   }
 }
